6ad38d3dd355b38b6de935b12468e85e09431c34
kshitiz91 <krohatgi18@gmail.com>
2018-09-12 11:10:32 +0530

    Initial structure for header

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# tais-react-app
+# empty-project-png-react-react-hot-loader-svg-sass-lodash
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-dev
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+Open the file `dist/index.html` in your browser
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+}
+
+module.exports = config;
