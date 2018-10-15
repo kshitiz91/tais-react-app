677e19c734ac4995eebb25dbaa6353e2a1dee2ab
Aashish Sachdeva <aashish.sachdeva@taistech.com>
2018-10-15 11:13:20 +0530

    homepage changes

--- src/body/categorylist/categorylisting/categorieslist/categoriesList.js
+++ src/body/categorylist/categorylisting/categorieslist/categoriesList.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { HyperLink } from '../../../../common/hyperLink';
+import { ImageArea } from '../../../../common/imageArea';
+import './categoriesList.scss'
 
 // Custom Categories List for homepage (funtional Component),  you need to pass all attributes values in props
 // Added the common properties needed for a hyperlink
 export function HomeCategoryList (props){
     // const categoryHeadText = props.text ? props.text : 'Shop By Categories';
     const list = props.categories
-    alert(list.length);
+    //alert(list.length);
+    const displayCat = list.map((cat, index) =>
+        <div key={index} className="col-sm-3 catListItem">
+          <HyperLink url="#" title="category" className="catListLink">
+            <ImageArea alt="" height="auto" src="//raderain.sirv.com/T-Giant/Cat_Undershirts_588x400.png?format=webp" className="catListImg"/>
+          </HyperLink>
+          <p>{cat}</p>
+        </div>);
     return (
-      <ul>
-          {list.map(function(name, index){
-              return <li key={ index }>{name}</li>;
-            })}
-      </ul>
-    )
+      displayCat
+    );
 
 }
