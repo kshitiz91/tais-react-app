df3395d7d1551f53203130095980977ef29c966d
kshitiz91 <krohatgi18@gmail.com>
2018-10-29 23:05:18 +0530

    updated reducer object

--- src/templates/CategoryListingPage/categoryListingPage.reducer.js
+++ src/templates/CategoryListingPage/categoryListingPage.reducer.js
@@ -1,12 +1,12 @@
 import * as actionType from './categoryListingPage.actionType';
 import {State} from './categoryListingPage.model';
 
-const initialState: State = [];
+const initialState: State = { subCategoryList:[]};
 
 export default function categoryListReducer(state = initialState, action){
 	switch(action.type){
 		case actionType.FETCH_SUBCATEGORY_LIST_SUCCESS :
-			return  action.subCategoryList;
+			return  {...state , subCategoryList:action.subCategoryList};
 		default:
 			return state;
 	}
